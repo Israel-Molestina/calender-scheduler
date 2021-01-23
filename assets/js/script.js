2bb9554f42357eeb2d96811b7b2d0c1ff1ba9f9f
Israel Molestina <molestinaisrael@gmail.com>
2021-01-23 10:28:24 -0500

    added if statemetns to add past present and future classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,39 @@
 let DateTime = luxon.DateTime;
 
 let today = DateTime.local();
+let hour = today.hour;
+console.log(hour);
 console.log(today);
 
 // variable that holds objects for the times. 
 var hours = [
 
-    {hour: "9ma"},
+    {hour: "9am",
+     mhour: "9"},
 
-    {hour: "10am"},
+    {hour: "10am",
+     mhour: "10"},
 
-    {hour: "11am"},
+    {hour: "11am",
+     mhour: "11"},
 
-    {hour: "12pm"},
+    {hour: "12pm",
+     mhour: "12"},
 
-    {hour: "1pm"},
+    {hour: "1pm",
+     mhour: "13"},
 
-    {hour: "2pm"},
+    {hour: "2pm",
+     mhour: "14"},
 
-    {hour: "3pm"},
+    {hour: "3pm",
+     mhour: "15"},
 
-    {hour: "4pm"},
+    {hour: "4pm",
+     mhour: "16"},
 
-    {hour: "5pm"},
+    {hour: "5pm",
+     mhour: "17"},
 
 ]
 
@@ -56,7 +67,23 @@ $(document).ready(function() {
         var btnSec = $("<button>").addClass("saveBtn col-2");
         row.append(btnSec);
 
-    })
+        if (hour < hours.mhour) {
+            $(textSec).addClass("future");
+        }
+
+        if (hour > hours.mhour) {
+            $(textSec).addClass("past");
+        }
+
+        else if (hour = hours.mhour) {
+            $(textSec).addClass("present");
+        }
+
+    });
+
+    
 
 });
+
+
 
